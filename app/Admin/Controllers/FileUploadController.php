02bf00febda43b15d6310e25b3e233cd74be8452
Chimarrao <lucas1998lc@gmail.com>
2023-11-22 22:44:40 -0300

    Adicionado exceção para imagens gif no editor

--- app/Admin/Controllers/FileUploadController.php
+++ app/Admin/Controllers/FileUploadController.php
@@ -12,14 +12,23 @@ class FileUploadController
     {
         if ($request->file('file')) {
             $file = $request->file('file');
-            $filename = $file->getClientOriginalName() . '-' . uniqid() . '.webp';
-    
-            $image = Image::make($file)->encode('webp', 75);
-            $image->save(public_path('images/' . $filename));
-    
-            $fileUrl = asset('images/' . $filename);
-    
-            return json_encode(['location' => $fileUrl]);
+
+            if ($file->getClientOriginalExtension() === 'gif') {
+                $filename = $file->getClientOriginalName() . '-' . uniqid() . '.gif';
+                $file->move(public_path('images/'), $filename);
+                $fileUrl = asset('images/' . $filename);
+
+                return json_encode(['location' => $fileUrl]);
+            } else {
+                $filename = $file->getClientOriginalName() . '-' . uniqid() . '.webp';
+
+                $image = Image::make($file)->encode('webp', 80);
+                $image->save(public_path('images/' . $filename));
+
+                $fileUrl = asset('images/' . $filename);
+
+                return json_encode(['location' => $fileUrl]);
+            }
         }
     }
 }
