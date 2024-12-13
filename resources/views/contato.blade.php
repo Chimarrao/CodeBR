f63aef3c00a033fb0e6780426e7ae192222f74ec
Chimarrao <lucas1998lc@gmail.com>
2024-12-13 15:01:21 -0300

    Alterado versão do bundle nos arquivos

--- resources/views/contato.blade.php
+++ resources/views/contato.blade.php
@@ -57,8 +57,8 @@
     <div id="app"></div>
 </body>
 
-<link href="{{ asset('css/app.css') }}?v=1.0.1" rel="stylesheet">
-<script src="{{ asset('js/bundle.js') }}?v=1.0.1"></script>
+<link href="{{ asset('css/app.css') }}?v=1.1.1" rel="stylesheet">
+<script src="{{ asset('js/bundle.js') }}?v=1.1.1"></script>
 
 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3072419723964148" crossorigin="anonymous"></script>
 
