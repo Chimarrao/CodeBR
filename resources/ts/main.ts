cd99becece9286bea95433d5cd4281f86246cbca
Chimarrao <lucas1998lc@gmail.com>
2025-01-07 16:32:49 -0300

    Removido chamadas a arquivos inexistentes

--- resources/ts/main.ts
+++ resources/ts/main.ts
@@ -11,9 +11,7 @@ app.mount('#app');
 
 import { alerts } from './alerts/alerts';
 
-// import './contato';
 import './modoDark';
-// import './particulas';
 import './menuHamburguer';
 
 import '@fortawesome/fontawesome-free/css/all.css';
