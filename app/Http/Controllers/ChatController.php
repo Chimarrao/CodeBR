1d2fb2e8a2326e87a1f891531cbccae65a3e0e27
Chimarrao <lucas1998lc@gmail.com>
2025-02-08 00:48:32 -0300

    Removida versão sem stream do chat

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -10,31 +10,6 @@ use OpenAI;
 
 class ChatController extends Controller
 {
-    public function chat2(Request $request)
-    {
-        $request->validate([
-            'prompt' => 'required|string',
-        ]);
-
-        $client = OpenAI::factory()
-            ->withApiKey(env('OPENAI_API_KEY')) 
-            ->withBaseUri('https://fresedgpt.space/v1') 
-            ->withHttpClient(new \GuzzleHttp\Client(['verify' => false]))
-            ->make();
-        
-        $response = $client->chat()->create([
-            'messages' => [
-                ['role' => 'user', 'content' => $request->input('prompt')],
-            ],
-            'model' => 'gpt-4o', 
-            'stream' => false, 
-        ]);
-
-        return response()->json([
-            'response' => $response['choices'][0]['message']['content'],
-        ]);
-    }
-
     public function chat(Request $request)
     {
         $request->validate([
